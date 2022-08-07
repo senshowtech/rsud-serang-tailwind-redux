eb711a57ca672467ec0aaefa79670a991085cde8
sena <hargiantofebrisena@gmail.com>
2022-08-07 12:05:01 +0700

    initial

--- src/component/HomeUser/CallDocter.js
+++ src/component/HomeUser/CallDocter.js
@@ -28,7 +28,7 @@ export default function CallDocter() {
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
               <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Pesan Homecare
+                Layanan Homecare
               </h3>
               <p className="mt-1 text-sm text-gray-600">
                 Form Pengisian data diri Pasien
@@ -45,7 +45,7 @@ export default function CallDocter() {
                         htmlFor="first-name"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        First name
+                        Nama Depan
                       </label>
                       <input
                         type="text"
@@ -61,7 +61,7 @@ export default function CallDocter() {
                         htmlFor="last-name"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        Last name
+                        Nama Belakang
                       </label>
                       <input
                         type="text"
@@ -77,7 +77,7 @@ export default function CallDocter() {
                         htmlFor="email-address"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        Email address
+                        Nomor Telpon/Handphone
                       </label>
                       <input
                         type="text"
@@ -88,7 +88,7 @@ export default function CallDocter() {
                       />
                     </div>
 
-                    <div className="col-span-6 sm:col-span-3">
+                    {/* <div className="col-span-6 sm:col-span-3">
                       <label
                         htmlFor="country"
                         className="block text-sm font-medium text-gray-700"
@@ -105,68 +105,20 @@ export default function CallDocter() {
                         <option>Canada</option>
                         <option>Mexico</option>
                       </select>
-                    </div>
+                    </div> */}
 
                     <div className="col-span-6">
                       <label
                         htmlFor="street-address"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        Street address
+                        Alamat
                       </label>
                       <input
                         type="text"
                         name="street-address"
                         id="street-address"
                         autoComplete="street-address"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                      <label
-                        htmlFor="city"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        City
-                      </label>
-                      <input
-                        type="text"
-                        name="city"
-                        id="city"
-                        autoComplete="address-level2"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="region"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        State / Province
-                      </label>
-                      <input
-                        type="text"
-                        name="region"
-                        id="region"
-                        autoComplete="address-level1"
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                      <label
-                        htmlFor="postal-code"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        ZIP / Postal code
-                      </label>
-                      <input
-                        type="text"
-                        name="postal-code"
-                        id="postal-code"
-                        autoComplete="postal-code"
                         className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
@@ -177,7 +129,7 @@ export default function CallDocter() {
                     type="submit"
                     className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   >
-                    Pesan
+                    Kirim
                   </button>
                 </div>
               </div>
